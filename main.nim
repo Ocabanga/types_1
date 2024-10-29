c91ad5e88f2b8f7a78a81778a8e535745401e8a7
Ocabanga <113467499+Ocabanga@users.noreply.github.com>
2024-10-29 22:46:44 +0300

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,8 +1,59 @@
+# main.nim
 type
-    # Для каждого объекта и атрибута после названия добавьте звездочку.
-    # Код для приюта (Shelter)
+    # Структуры для приюта (Shelter)
+    Manager* = object
+        name*: string
+        post*: string
+
+    Staff* = object
+        name*: string
+        birthDate*: string
+        uid*: int
+
+    Pet* = object
+        name*: string
+        age*: int
+
     Shelter* = object
+        manager*: Manager
+        staff*: Staff
+        pet*: Pet
 
-    # Код для школы (School)
+    # Структуры для школы (School)
+    Director* = object
+        name*: string
 
-    # Код для магазина (Shop)
+    Teacher* = object
+        name*: string
+        class*: string
+
+    Student* = object
+        name*: string
+        class*: string
+
+    School* = object
+        director*: Director
+        teacher*: Teacher
+        student*: Student
+
+    # Структуры для магазина (Shop)
+    ShopManager* = object
+        name*: string
+
+    ShopStaff* = object
+        name*: string
+        birthDate*: string
+        post*: string
+
+    Good* = object
+        title*: string
+        endDate*: string
+
+    Cash* = object
+        number*: int
+
+    Shop* = object
+        manager*: ShopManager
+        staff*: ShopStaff
+        good*: Good
+        cash*: Cash
